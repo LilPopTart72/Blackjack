ce2aba8312456fde565570f4f2d25592621f3a41
LilPopTart72 <rilleythompson0220@gmail.com>
2025-04-16 02:32:02 -0500

    Revamped For Future Scaling

--- War.py
+++ War.py
@@ -4,6 +4,7 @@ from PyQt5.QtGui import QPixmap
 from PyQt5 import uic 
 import sys
 import random
+from others import *
 
 # GUI Class For War
 class UI(QMainWindow):
@@ -42,95 +43,54 @@ class UI(QMainWindow):
     def shuffle(self):
         
         # Sets Up The Deck And Shuffle
-        self.deck = [f"{y}_of_{x}" for x in ["diamonds", "clubs", "hearts", "spades"] for y in range(2, 15)]
-        random.shuffle(self.deck) 
-        
-        # Deals Half To Dealer Half To Player
-        self.dealer, self.player = self.deck[:26], self.deck[26:] 
+        self.game = Game()
         
         self.setWindowTitle("The Game Of War")
-        self.dealerHandLabel.setText(str(len(self.dealer)))
-        self.playerHandLabel.setText(str(len(self.player)))
+        self.dealerHandLabel.setText(str(len(self.game.dealer.hand)))
+        self.playerHandLabel.setText(str(len(self.game.player.hand)))
         
         for i in ["playerLabel", "dealerLabel"]:
                 getattr(self,i).setPixmap(QPixmap())
                 
     def dealCards(self):
         
-        # Clears War Cards
-        for i in [self.cWar_1, self.cWar_2, self.cWar_3, self.cWar_4, self.cWar_5, self.cWar_6]:
-            i.setPixmap(QPixmap())
-        
         # Deals and Scores Both Players Cards
         try:
-            
+            # Resets War Var
+            if self.game.war == True:
+                self.game.war = False
             # Grab a Card For players
-            dcard = self.dealer.pop(0)
-            pcard = self.player.pop(0)
-            
-            # Displays Cards
-            self.dealerLabel.setPixmap(QPixmap(f'images/cards/{dcard}.png'))
-            self.playerLabel.setPixmap(QPixmap(f'images/cards/{pcard}.png'))
+            dcard = self.game.dealer.hand.pop(0)
+            pcard = self.game.player.hand.pop(0)
             
             # Score The Cards On Screen
-            self.Score(pcard, dcard, [pcard, dcard])
-            
-            # Update Hand Label
-            self.dealerHandLabel.setText(str(len(self.dealer)))
-            self.playerHandLabel.setText(str(len(self.player)))
+            self.game.Score(pcard, dcard, [pcard, dcard])
+
+            # Updates the widgets
+            self.Update(pcard, dcard)
         
         # Someone Has Won / Lost
         except IndexError:
             self.setWindowTitle("Game Over")
+        
+    def Update(self, pcard, dcard):
+        # Update Hand Label
+            self.dealerLabel.setPixmap(QPixmap(dcard.image))
+            self.playerLabel.setPixmap(QPixmap(pcard.image))
+            self.dealerHeaderLabel.setText(self.game.dealersmsg)
+            self.playerHeaderLabel.setText(self.game.playermsg)
+            self.dealerHandLabel.setText(str(len(self.game.dealer.hand)))
+            self.playerHandLabel.setText(str(len(self.game.player.hand)))
             
-    def Score(self, pcard, dcard, spoils):
-        # Grabs The Number Value Of The Card
-        pcard_value = int(pcard[:2].strip('_'))
-        dcard_value = int(dcard[:2].strip('_'))
-
-        # Returns Who Won
-        result = "Player Wins" if pcard_value > dcard_value else "Dealer Wins" if pcard_value < dcard_value else "War"
-        
-        # Updates Score Or Proceeds To War
-        if result == "War":
-            self.War(spoils)
-        else:
-            self.update_score(result, spoils)
-
-    def update_score(self, result, spoils):
-        
-        # Initializes A Dictionary For Possible Win / Lose Scenarios
-        actions = {
-            "Player Wins": ("Dealer Loses", "Player Wins", self.player),
-            "Dealer Wins": ("Dealer Wins", "Player Loses", self.dealer),
-        }
-        
-        # Gives The Winner Both Cards
-        dealer_text, player_text, winner = actions.get(result, (None, None, None))
-        if dealer_text and player_text:
-            self.dealerHeaderLabel.setText(dealer_text)
-            self.playerHeaderLabel.setText(player_text)
-            winner.extend(spoils)
-    
-    def War(self, previous):
-        
-        # Displays The Next 3 Cards For Each Player
-        for i in range(3):
-            getattr(self, f"cWar_{i+1}").setPixmap(QPixmap(f'images/cards/{self.player[i]}.png'))
-            getattr(self, f"cWar_{i+4}").setPixmap(QPixmap(f'images/cards/{self.dealer[i]}.png'))
-        
-        # Collect Spoils And Remove Cards From Decks
-        spoils = self.dealer[:3] + self.player[:3] + previous
-        pcard = self.player[2]
-        dcard = self.dealer[2]
-        
-        # Update Decks By Removing The War Cards
-        self.player = self.player[3:]
-        self.dealer = self.dealer[3:]
-        
-        # Score The Final War Card
-        self.Score(pcard, dcard, spoils)
-        
+            if self.game.war == True:
+                # Displays The Next 3 Cards For Each Player
+                for i in range(3):
+                    getattr(self, f"cWar_{i+1}").setPixmap(QPixmap(self.game.war_cards[i+3].image))
+                    getattr(self, f"cWar_{i+4}").setPixmap(QPixmap(self.game.war_cards[i].image))
+            else:
+                # Clears War Cards
+                for i in [self.cWar_1, self.cWar_2, self.cWar_3, self.cWar_4, self.cWar_5, self.cWar_6]:
+                    i.setPixmap(QPixmap())
 # Initialize The App
 app = QApplication(sys.argv)
 UIWindow = UI()
--- others.py
+++ others.py
@@ -1,3 +1,5 @@
+import random
+
 class Card:
     def __init__(self, rank, suit):
         self.rank = rank
@@ -16,9 +18,9 @@ class Deck:
         return self.cards.pop(0)
 
 class Player:
-    def __init__(self, name):
+    def __init__(self, name, hand):
         self.name = name
-        self.hand = []
+        self.hand = hand
 
     def add_cards(self, cards):
         self.hand.extend(cards)
@@ -26,19 +28,57 @@ class Player:
 class Game:
     def __init__(self):
         self.deck = Deck()
-        self.player = Player("Player")
-        self.dealer = Player("Dealer")
+        self.player = Player("Player", self.deck.cards[26:])
+        self.dealer = Player("Dealer", self.deck.cards[:26])
+        self.playermsg = "Players Hand"
+        self.dealersmsg = "Dealers Hand"
+        self.war = False
+        self.war_cards = []
 
-    def play_round(self):
-        player_card = self.player.hand.pop(0)
-        dealer_card = self.dealer.hand.pop(0)
-        winner = self.determine_winner(player_card, dealer_card)
-        self.handle_winner(winner, player_card, dealer_card)
 
-    def determine_winner(self, player_card, dealer_card):
-        if player_card.rank > dealer_card.rank:
-            return "Player"
-        elif player_card.rank < dealer_card.rank:
-            return "Dealer"
+    def Score(self, pcard, dcard, spoils):
+        # Grabs The Number Value Of The Card
+        pcard_value = int(pcard.rank)
+        dcard_value = int(dcard.rank)
+        print(dcard_value)
+        print(pcard_value)
+        # Returns Who Won
+        result = "Player Wins" if pcard_value > dcard_value else "Dealer Wins" if pcard_value < dcard_value else "War"
+        # Updates Score Or Proceeds To War
+        if result == "War":
+            self.War(spoils)
         else:
-            return "War"
+            self.update_score(result, spoils)
+            
+
+    def update_score(self, result, spoils):
+        
+        # Initializes A Dictionary For Possible Win / Lose Scenarios
+        actions = {
+            "Player Wins": ("Dealer Loses", "Player Wins", self.player),
+            "Dealer Wins": ("Dealer Wins", "Player Loses", self.dealer),
+        }
+        
+        # Gives The Winner Both Cards
+        dealer_text, player_text, winner = actions.get(result, (None, None, None))
+        if dealer_text and player_text:
+            winner.hand.extend(spoils)
+            self.playermsg = player_text
+            self.dealersmsg = dealer_text
+    
+    def War(self, previous):
+        
+        # Sets War To True
+        self.war = True
+        
+        # Collect Spoils And Remove Cards From Decks
+        self.war_cards = self.dealer.hand[:3] + self.player.hand[:3] + previous
+        pcard = self.player.hand[2]
+        dcard = self.dealer.hand[2]
+        
+        # Update Decks By Removing The War Cards
+        self.player.hand = self.player.hand[3:]
+        self.dealer.hand = self.dealer.hand[3:]
+        
+        # Score The Final War Card
+        self.Score(pcard, dcard, self.war_cards)
